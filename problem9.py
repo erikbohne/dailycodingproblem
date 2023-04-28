f2080097b9c2664704c0b6475b948e7f5f9f3cca
Erik Bohne <eriknbohne@gmail.com>
2023-04-28 08:38:51 +0200

    #10 problem

---
+++ problem9.py
@@ -0,0 +1,17 @@
+"""
+april 28th 2023
+
+This problem was asked by Apple.
+
+Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
+"""
+from time import sleep
+
+def scheduler(function, ms):
+    sleep(1e-3*ms)
+    function()
+    
+def test():
+    print("hello, world!")
+    
+scheduler(test, 3000)
